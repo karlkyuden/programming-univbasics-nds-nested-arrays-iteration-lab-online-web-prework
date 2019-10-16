38203bf71bcde41e4c7a8cc41751a5acf1044bf1
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-16 03:08:35 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,6 +1,6 @@
 def join_ingredients(src)
-  string_array = Array.new
   iter = 0
+  newString = 
   while iter < src.length do
     string_array.push("I love #{src[iter][0]} and #{src[iter][1]} on my pizza")
     iter += 1
